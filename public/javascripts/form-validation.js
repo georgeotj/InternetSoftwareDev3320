19728faff28a72e2badc5d97f89976c27ed63123
TrevorMcdougald <trevor.mcdougald@gmail.com>
2019-10-13 20:30:09 -0500

    I finally added a javascript component. I need to add many more.

--- public/javascripts/form-validation.js
+++ public/javascripts/form-validation.js
@@ -1,23 +1,44 @@
-// eslint-disable-next-line no-multi-assign
-window.$ = window.jQuery = jQuery;
+$(function() {
+  $("#address-1-error-message").hide();
+  $("#address-2-error-message").hide();
+  $("#shipping-state-error-message").hide();
+  $("#city-shipping-error-message").hide();
+  $("#shipping-zip-error-message").hide();
 
-$(document).ready(() => {
-  $('.shipping-info-form').validate({
-    rules: {
-      address_1: 'required',
-      address_2: 'optional',
-      city: 'required',
-      user_state: 'required',
-      zipcode: 'required',
-    },
-    messages: {
-      address_1: 'Address is required',
-      city: 'City is required',
-      user_state: 'State is required',
-      zipcode: 'Zip Code is required',
-    },
-    submitHandler(form) {
-      form.submit();
-    },
-  });
-});
+  let error_address_1 = false;
+  let error_address_2 = false;
+  let error_state = false;
+  let error_city = false;
+  let error_zip = false;
+
+})
+
+
+
+
+
+
+
+// // eslint-disable-next-line no-multi-assign
+// window.$ = window.jQuery = jQuery;
+//
+// $(document).ready(() => {
+//   $('.shipping-info-form').validate({
+//     rules: {
+//       address_1: 'required',
+//       address_2: 'optional',
+//       city: 'required',
+//       user_state: 'required',
+//       zipcode: 'required',
+//     },
+//     messages: {
+//       address_1: 'Address is required',
+//       city: 'City is required',
+//       user_state: 'State is required',
+//       zipcode: 'Zip Code is required',
+//     },
+//     submitHandler(form) {
+//       form.submit();
+//     },
+//   });
+// });
