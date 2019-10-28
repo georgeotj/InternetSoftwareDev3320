13528c412370a8fe0d7a0a6d058a114663960d2e
TrevorMcdougald <trevor.mcdougald@gmail.com>
2019-10-28 13:18:38 -0500

    A cleaner implementation for loading our application with static files.

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const path = require( 'path' );
 
 
-
 const port = process.env.PORT || 3000;
 
 const router = express.Router();
+
+// Add .css and .js static files to the app
+app.use( '/public', express.static( path.join( __dirname, '/public' ) ) );
 
 // Add path to HTML file to the router
 router.get( '/', ( request, response ) => {
@@ -16,64 +18,6 @@ router.get( '/', ( request, response ) => {
   // sendfile(): sends HTML files to the browser
   response.sendFile( path.join( __dirname, '/index.html' ) );
 });
-
-router.get( '/public/stylesheets/style.css', ( request, response ) => {
-
-  // __dirname: resolves to project folder
-  // sendfile(): sends HTML files to the browser
-  response.sendFile( path.join( __dirname, '/public/stylesheets/style.css' ) );
-});
-
-router.get( '/public/stylesheets/style.css', ( request, response ) => {
-
-  // __dirname: resolves to project folder
-  // sendfile(): sends HTML files to the browser
-  response.sendFile( path.join( __dirname, '/public/stylesheets/style.css' ) );
-});
-
-router.get( '/public/javascripts/back-to-top-button.js', ( request, response ) => {
-
-  // __dirname: resolves to project folder
-  // sendfile(): sends HTML files to the browser
-  response.sendFile( path.join( __dirname, '/public/javascripts/back-to-top-button.js' ) );
-});
-
-router.get( '/public/javascripts/account-form-validation.js', ( request, response ) => {
-
-  // __dirname: resolves to project folder
-  // sendfile(): sends HTML files to the browser
-  response.sendFile( path.join( __dirname, '/public/javascripts/account-form-validation.js' ) );
-});
-
-router.get( '/public/javascripts/checkout-form-validation.js', ( request, response ) => {
-
-  // __dirname: resolves to project folder
-  // sendfile(): sends HTML files to the browser
-  response.sendFile( path.join( __dirname, '/public/javascripts/checkout-form-validation.js' ) );
-});
-
-router.get( '/public/javascripts/shipping-form-validation.js', ( request, response ) => {
-
-  // __dirname: resolves to project folder
-  // sendfile(): sends HTML files to the browser
-  response.sendFile( path.join( __dirname, '/public/javascripts/shipping-form-validation.js' ) );
-});
-
-router.get( '/public/javascripts/shopping-cart.js', ( request, response ) => {
-
-  // __dirname: resolves to project folder
-  // sendfile(): sends HTML files to the browser
-  response.sendFile( path.join( __dirname, '/public/javascripts/shopping-cart.js' ) );
-});
-
-
-
-
-
-// Add .css and .js static files to the app
-// app.use( '/style.css', express.static( path.join( __dirname, '/public/stylesheets' ) ) );
-// app.use( express.static( path.join( __dirname, '/public/javascripts/' ) ) );
-app.use( express.static( 'C:\\Users\\Trevor\\Web_Development\\InternetSoftwareDev3320\\public' ) );
 
 // Add the router to the application
 app.use( '/', router );
